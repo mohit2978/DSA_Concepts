8be472bb665bcda33cd01a797416e39cf0503261
mohit2978 <mohitrakheja007@gmail.com>
2025-04-18 00:01:41 +0530

    added tree creation

---
+++ Concepts/006.binaryTrees/tree.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public: Node * left;
+    Node * right;
+    int val;
+
+    Node(int val) {
+        this -> val = val;
+        this -> left = nullptr;
+        this -> right = nullptr;
+    }
+};
+
+Node * binaryTree() {
+
+    int x;
+    cout << "Enter node value" << "\n";
+    cin >> x;
+    if (x == -1) return nullptr;
+    Node * node = new Node(x);
+
+    cout << "Enter left value";
+    node -> left = binaryTree();
+
+    cout << "Enter right value";
+    node -> right = binaryTree();
+
+    return node;
+
+}
+
+void inorderTraversal(vector < int > & vec, Node * node) {
+    if (node == nullptr) return;
+
+    inorderTraversal(vec, node -> left);
+    vec.push_back(node -> val);
+    inorderTraversal(vec, node -> right);
+}
+
+void print(vector < int > & v) {
+
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << ",";
+    }
+}
+
+int main() {
+    Node * root = binaryTree();
+
+    vector < int > vec;
+    inorderTraversal(vec, root);
+    print(vec);
+    return 0;
+}
